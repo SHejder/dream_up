21d2f22fedb4ba527811da0fcc6ed1c59c1ad9d7
SHejder <syhanov.m@yandex.ru>
2018-02-28 09:56:50 +0300

    вывел инпуты для адреса

--- components/com_jshopping/templates/dream_up/user/login.php
+++ components/com_jshopping/templates/dream_up/user/login.php
@@ -10,6 +10,10 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 <!--<div class = "jshop pagelogin" id="comjshop">    -->
 <?php //print $this->checkout_navigator ?>
+<?php
+$config_fields = $this->config_fields;
+?>
+
 
 <h1><?php print _JSHOP_LOGIN ?></h1>
 
@@ -17,10 +21,9 @@ defined('_JEXEC') or die('Restricted access');
 <div class="ordering">
     <?php
     $messages = JFactory::getApplication()->getMessageQueue();
-    if (is_array($messages))
-    {
+    if (is_array($messages)) {
         echo '<div class="warnings">';
-        foreach($messages as $message) echo '<div class="'.$message['type'].'">'.$message['message'].'</div>';
+        foreach ($messages as $message) echo '<div class="' . $message['type'] . '">' . $message['message'] . '</div>';
         echo '</div>';
     };
     ?>
@@ -52,7 +55,7 @@ defined('_JEXEC') or die('Restricted access');
                 </div>
                 <div class="custom-checkbox">
                     <label class="required">
-                        <input type="checkbox" name="remember" value="1"  checked="">
+                        <input type="checkbox" name="remember" value="1" checked="">
                         <span><?php print _JSHOP_REMEMBER_ME ?></span>
                     </label>
                 </div>
@@ -76,91 +79,157 @@ defined('_JEXEC') or die('Restricted access');
         </div>
     <?php endif; ?>
     <?php if ($this->config->shop_user_guest && $this->show_pay_without_reg) : ?>
-        <div class="ordering__item">
-            <button class="ordering__item-title"><span>Я хочу оформить заказ БЕЗ регистрации</span></button>
-            <div class="ordering__item-content form">
+    <div class="ordering__item">
+        <button class="ordering__item-title"><span>Я хочу оформить заказ БЕЗ регистрации</span></button>
+        <div class="ordering__item-content form">
 
-                    <form action = "/katalog/checkout/step2save" method = "post" name = "loginForm" onsubmit = "return validateCheckoutAdressForm('<?php print $this->live_path ?>', this.name)" class = "form-horizontal" autocomplete="off" enctype="multipart/form-data">
-                        <?php print $this->_tmp_ext_html_address_start?>
+            <form action="/katalog/checkout/step2save" method="post" name="loginForm"
+                  onsubmit="return validateCheckoutAdressForm('<?php print $this->live_path ?>', this.name)"
+                  class="form-horizontal" autocomplete="off" enctype="multipart/form-data">
+                <?php print $this->_tmp_ext_html_address_start ?>
 
-                        <?php if ($config_fields['l_name']['display']){?>
-                            <div class="form__item -name has-content">
-                                <label>Фамилия</label>
-                                <input type = "text" name = "l_name" placeholder="Фамилия" id = "l_name" value = "<?php print $this->user->l_name ?>" />
-                                <div class="form__item-error"></div>
-                            </div>
-                        <?php } ?>
-                        <?php if ($config_fields['f_name']['display']){?>
-                            <div class="form__item -name has-content">
-                                <label>Имя</label>
-                                <input type = "text" name = "f_name" id = "f_name" placeholder="Имя" value = "<?php print $this->user->f_name ?>" />
-                                <div class="form__item-error"></div>
-                            </div>
-                        <?php } ?>
-                        <?php if ($config_fields['m_name']['display']){?>
+                <?php if ($config_fields['l_name']['display']) { ?>
+                    <div class="form__item -name has-content">
+                        <label>Фамилия</label>
+                        <input type="text" name="l_name" placeholder="Фамилия" id="l_name"
+                               value="<?php print $this->user->l_name ?>"/>
+                        <div class="form__item-error"></div>
+                    </div>
+                <?php } ?>
+                <?php if ($config_fields['f_name']['display']) { ?>
+                    <div class="form__item -name has-content">
+                        <label>Имя</label>
+                        <input type="text" name="f_name" id="f_name" placeholder="Имя"
+                               value="<?php print $this->user->f_name ?>"/>
+                        <div class="form__item-error"></div>
+                    </div>
+                <?php } ?>
+                <?php if ($config_fields['m_name']['display']) { ?>
 
-                            <div class="form__item -name has-content">
-                                <label>Отчество</label>
-                                <input type = "text" name = "m_name" id = "m_name" placeholder="Отчество" value = "<?php print $this->user->m_name ?>" />
-                                <div class="form__item-error"></div>
-                            </div>
-                        <?php } ?>
-                        <?php if ($config_fields['email']['display']){?>
-                            <div class="form__item -name has-content">
-                                <label>Email</label>
-                                <input type = "text" name = "email" placeholder="E-mail" id = "email" value = "<?php print $this->user->email ?>"  />
-                                <div class="form__item-error"></div>
-                            </div>
-                        <?php } ?>
-                        <?php echo $this->_tmpl_address_html_2?>
+                    <div class="form__item -name has-content">
+                        <label>Отчество</label>
+                        <input type="text" name="m_name" id="m_name" placeholder="Отчество"
+                               value="<?php print $this->user->m_name ?>"/>
+                        <div class="form__item-error"></div>
+                    </div>
+                <?php } ?>
+                <?php if ($config_fields['email']['display']) { ?>
+                    <div class="form__item -name has-content">
+                        <label>Email</label>
+                        <input type="text" name="email" placeholder="E-mail" id="email"
+                               value="<?php print $this->user->email ?>"/>
+                        <div class="form__item-error"></div>
+                    </div>
+                <?php } ?>
+                <?php echo $this->_tmpl_address_html_2 ?>
 
-                        <?php if ($config_fields['mobil_phone']['display']){?>
+                <?php if ($config_fields['mobil_phone']['display']) { ?>
 
-                            <div class="form__item -name has-content">
-                                <label>Мобильный телефон</label>
-                                <input type = "text" name = "mobil_phone" placeholder="Телефон" id = "mobil_phone" value = "<?php print $this->user->mobil_phone ?>" />
-                                <div class="form__item-error"></div>
-                            </div>
-                        <?php } ?>
-                        <?php echo $this->_tmpl_address_html_3?>
+                    <div class="form__item -name has-content">
+                        <label>Мобильный телефон</label>
+                        <input type="text" name="mobil_phone" placeholder="Телефон" id="mobil_phone"
+                               value="<?php print $this->user->mobil_phone ?>"/>
+                        <div class="form__item-error"></div>
+                    </div>
+                <?php } ?>
+                <?php echo $this->_tmpl_address_html_3 ?>
+                <!--                        --><?php //if ($this->count_filed_delivery > 0){?>
+                <div class="form__item -radio">
+                    <label>Доставка на ваш адрес?
+                        <label class="custom-radio">
+                            <input type="radio" name="delivery_adress" id="delivery_adress_1"
+                                   value="0" <?php if (!$this->delivery_adress) { ?> checked="checked" <?php } ?>
+                                   onclick="jQuery('#div_delivery').hide()"/>
+                            <span><?php print _JSHOP_NO ?></span>
 
+                        </label>
+                        <label class="custom-radio">
+                            <input type="radio" name="delivery_adress" id="delivery_adress_2"
+                                   value="1" <?php if ($this->delivery_adress) { ?> checked="checked" <?php } ?>
+                                   onclick="jQuery('#div_delivery').show()"/>
+                            <span><?php print _JSHOP_YES ?></span>
 
-                        <div class="custom-checkbox">
-                            <label class="required">
-                                <input type="checkbox" value="1" required="" checked="">
-                                <span>Я согласен на обработку персональных данных. <a href="">Политика конфиденциальности</a></span>
-                            </label>
-                        </div>
-                        <button  class="homepage-bottom__form-btn btn" type="submit">Отправить</button>
-                    </form>
+                        </label>
+                    </label>
+
                 </div>
 
-            </div>
-        </div>
-    <?php endif; ?>
+                <!--                        --><?php //}?>
 
-    <?php if ($this->config->shop_user_guest && !$this->show_pay_without_reg) : ?>
-        <!--    </div>-->
-        <div class="ordering__item">
-
-
-            <?php echo $this->tmpl_login_html_4 ?>
-            <?php if ($this->allowUserRegistration) { ?>
-                <?php if (!$this->config->show_registerform_in_logintemplate) { ?>
-                    <span class="small_header"><?php print _JSHOP_HAVE_NOT_ACCOUNT ?>?</span>
-                    <div class="logintext">
-                        <a href="<?php print $this->href_register ?>">Зарегистрируйтесь</a> пожалуйста!
-<!--                        --><?php //print _JSHOP_REGISTER ?>
+                <div id="div_delivery" class="jshop_register"
+                     style="padding-bottom:0px;<?php if (!$this->delivery_adress) { ?>display:none;<?php } ?>">
+                    <?php echo $this->_tmpl_address_html_5 ?>
+                    <div class="form__item">
+                        <label><?php print _JSHOP_ZIP ?> *</label>
+                        <input type="text" name="d_zip" id="d_zip" value="<?php print $this->user->d_zip ?>"/>
                     </div>
-                <?php } else { ?>
-                    <?php $hideheaderh1 = 1;
-                    include(dirname(__FILE__) . "/register.php"); ?>
-                <?php } ?>
-            <?php } ?>
-            <?php echo $this->tmpl_login_html_5 ?>
+                    <div class="form__item">
+                        <label>Регион *</label>
+                            <input type="text" name="d_state" id="d_state" value="<?php print $this->user->d_state ?>" />
+                    </div>
+                    <div class="form__item">
+                        <label>Населённый пункт *</label>
+                            <input type="text" name="d_city" id="d_city" value="<?php print $this->user->d_city ?>"
+                                   />
+                        </div>
+                    <div class="form__item">
+                        <label>Улица *</label>
+
+                            <input type="text" name="d_street" id="d_street"
+                                   value="<?php print $this->user->d_street ?>" />
+                        </div>
+                    <div class="form__item">
+                        <label><?php print _JSHOP_HOME ?> *</label>
+                            <input type="text" name="d_home" id="d_home" value="<?php print $this->user->d_home ?>"
+                                   />
+                        </div>
+                    <div class="form__item">
+                        <label><?php print _JSHOP_APARTMENT ?> *</label>
+
+                            <input type="text" name="d_apartment" id="d_apartment"
+                                   value="<?php print $this->user->d_apartment ?>" />
+                        </div>
+                    <?php echo $this->_tmpl_address_html_6 ?>
+                </div>
+
+
+                <div class="custom-checkbox">
+                    <label class="required">
+                        <input type="checkbox" value="1" required="" checked="">
+                        <span>Я согласен на обработку персональных данных. <a
+                                    href="">Политика конфиденциальности</a></span>
+                    </label>
+                </div>
+                <button class="homepage-bottom__form-btn btn" type="submit">Отправить</button>
+            </form>
         </div>
-    <?php endif; ?>
+
+    </div>
+</div>
+<?php endif; ?>
+
+<?php if ($this->config->shop_user_guest && !$this->show_pay_without_reg) : ?>
+    <!--    </div>-->
+    <div class="ordering__item">
 
 
-    <!--</div>-->
-    <?php echo $this->tmpl_login_html_6 ?>
+        <?php echo $this->tmpl_login_html_4 ?>
+        <?php if ($this->allowUserRegistration) { ?>
+            <?php if (!$this->config->show_registerform_in_logintemplate) { ?>
+                <span class="small_header"><?php print _JSHOP_HAVE_NOT_ACCOUNT ?>?</span>
+                <div class="logintext">
+                    <a href="<?php print $this->href_register ?>">Зарегистрируйтесь</a> пожалуйста!
+                    <!--                        --><?php //print _JSHOP_REGISTER ?>
+                </div>
+            <?php } else { ?>
+                <?php $hideheaderh1 = 1;
+                include(dirname(__FILE__) . "/register.php"); ?>
+            <?php } ?>
+        <?php } ?>
+        <?php echo $this->tmpl_login_html_5 ?>
+    </div>
+<?php endif; ?>
+
+
+<!--</div>-->
+<?php echo $this->tmpl_login_html_6 ?>

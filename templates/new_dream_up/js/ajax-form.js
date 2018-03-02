af774328b50d8a13d10a1d421c60146b2edff4b8
SHejder <syhanov.m@yandex.ru>
2018-03-02 14:26:00 +0300

    fix reinit

--- templates/new_dream_up/js/ajax-form.js
+++ templates/new_dream_up/js/ajax-form.js
@@ -19,22 +19,18 @@
 // });
 
 jQuery(document).ready(function () {
-    jQuery('body').on('submit', 'form[action][method].ajax', function (e) {
+       jQuery('body').on('submit', 'form[action][method].ajax', function (e) {
         e.preventDefault();
-
         var $form = jQuery(this);
-        // console.log($form.find('.product__buy'));
-
-        if ($form.data('submitted')) {
-            return;
-        }
-
 
         ajaxSubmit($form.data('submitted', true));
         $form.find('.product__buy').addClass('add_more');
         $form.find('.product__buy').val('ДОБАВИТЬ ЕЩЕ');
+        // return false;
 
     });
+
+
 
 });
 

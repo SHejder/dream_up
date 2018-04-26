010055143fe04b6f1923645bba7ddeef94e3543a
SHejder <syhanov.m@yandex.ru>
2018-04-26 08:52:05 +0300

    вывел системные сообщения

--- templates/new_dream_up/html/com_contact/contact/default_form.php
+++ templates/new_dream_up/html/com_contact/contact/default_form.php
@@ -14,6 +14,15 @@ JHtml::_('behavior.formvalidator');
 
 ?>
 <div class="contacts__form form">
+    <?php
+    $messages = JFactory::getApplication()->getMessageQueue();
+    if (is_array($messages)) {
+        echo '<div class="warnings">';
+        foreach ($messages as $message) echo '<div class="' . $message['type'] . '">' . $message['message'] . '</div>';
+        echo '</div>';
+    };
+    ?>
+
     <h2>Остались вопросы? Напишите нам!</h2>
     <form action="<?php echo JRoute::_('index.php'); ?>" method="post" class="form-validate form-horizontal well">
         <div class="col-1">
@@ -24,7 +33,7 @@ JHtml::_('behavior.formvalidator');
             </div>
             <div class="form__item -email-phone">
                 <label for="form__item-email-phone">Email*</label>
-                <input type="email" name="jform[contact_email]" id="form__item-email-phone"
+                <input required="required" type="email" name="jform[contact_email]" id="form__item-email-phone"
                        placeholder="Email">
                 <!--            <div class="error"><ul><li>Неверный формат</li></ul></div>-->
             </div>
@@ -50,7 +59,10 @@ JHtml::_('behavior.formvalidator');
             <input type="hidden" name="id" value="<?php echo $this->contact->slug; ?>"/>
             <?php echo JHtml::_('form.token'); ?>
         </div>
+
         <div class="g-recaptcha" data-sitekey="6LfJwE0UAAAAAFlrvixKHHqfR4fLdrjW1Y9H3spi"></div>
+        <input name="jform[contact_subject]" id="jform_contact_emailmsg" style="visibility: hidden" value="Новое сообщение" class="required invalid" size="60" required="required" aria-required="true" aria-invalid="true" type="text">
+
     </form>
 
 </div><!-- homepage-bottom__form end -->
